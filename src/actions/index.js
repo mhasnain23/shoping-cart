65fa57fc75fea2732ec7f7a2a8f26928d2b028c3
mhasnain23 <tryfrelancer23@gmail.com>
2024-09-15 10:03:11 +0500

    nit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,58 +1,55 @@
-'use server';
+"use server";
 
 import { signIn, signOut } from "@/auth";
 
 // get all products
 
 export async function fetchAllProducts() {
-    try {
-        const apiResponse = await fetch('https://dummyjson.com/products', {
-            method: 'GET',
-            cache: "no-store",
-        });
-        const data = await apiResponse.json();
+  try {
+    const apiResponse = await fetch("https://dummyjson.com/products", {
+      method: "GET",
+      cache: "no-store",
+    });
+    const data = await apiResponse.json();
 
-        return {
-            success: true,
-            data: data?.products,
-        }
-
-
-    } catch (e) {
-        console.log(e);
-        return {
-            success: false,
-            message: "Something went wrong! please try again",
-        }
-    }
+    return {
+      success: true,
+      data: data?.products,
+    };
+  } catch (e) {
+    console.log(e);
+    return {
+      success: false,
+      message: "Something went wrong! please try again",
+    };
+  }
 }
-
 
 export async function fetchProductDetails(currentProductID) {
-    try {
-        const apiResponse = await fetch(`https://dummyjson.com/products/${currentProductID}`, {
-            method: 'GET',
-            cache: "no-store",
-        });
-        const data = await apiResponse.json();
+  try {
+    const apiResponse = await fetch(
+      `https://dummyjson.com/products/${currentProductID}`,
+      {
+        method: "GET",
+        cache: "no-store",
+      }
+    );
+    const data = await apiResponse.json();
 
-        return data;
-
-
-    } catch (e) {
-        console.log(e);
-        return {
-            success: false,
-            message: "Something went wrong! please try again",
-        }
-    }
+    return data;
+  } catch (e) {
+    console.log(e);
+    return {
+      success: false,
+      message: "Something went wrong! please try again",
+    };
+  }
 }
 
-
 export async function loginAction() {
-    await signIn("github")
+  await signIn("github");
 }
 
 export async function logoutAction() {
-    await signOut();
+  await signOut();
 }
