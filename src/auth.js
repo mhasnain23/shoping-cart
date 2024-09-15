b68706369316b65263f3a6daecf0be675597f473
mhasnain23 <tryfrelancer23@gmail.com>
2024-09-15 10:09:13 +0500

    nit

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
+import Github from "next-auth/providers/github";
 
 export const {
     handlers: { GET, POST },
@@ -7,5 +7,5 @@ export const {
     signIn,
     signOut,
 } = NextAuth({
-    providers: [GitHub],
+    providers: [Github],
 });
